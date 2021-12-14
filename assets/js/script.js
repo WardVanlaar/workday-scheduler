8566d5b30e8d394341d2dd52e426bbf74d310cdc
Ward <wardv@tirf.ca>
2021-12-14 13:11:23 -0500

    added color coding

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+
+// to show current day
 // obtained from https://medium.com/@aleks.roslyakov/using-css-grid-jquery-making-a-daily-scheduler-pt-ii-7af7d239a55d
 let d = new Date();
 
@@ -5,9 +7,46 @@ let year = d.getFullYear();
 let month = d.getMonth()+1;
 let day = d.getDate();
 
-let totalDays = new Date(year, month, 0).getDate();
+    //let totalDays = new Date(year, month, 0).getDate();
 
 let output = (month<10 ? '0' : '') + month + '/' + (day<10 ? '0' : '') + day + '/' + year;
 
 $("#currentDay").text(`${output}`);
 
+
+// to color-code time of day
+// adapted from https://stackoverflow.com/questions/62462599/how-can-i-change-elements-style-based-on-the-time-of-day
+
+var checkTime = function () {
+
+    //Get Current time
+    var currentTime = moment().format('H');
+
+    //get all one-hour time blocks with class "time-block"
+    var timeBlockElements = $(".time-block");
+
+    //loop through time block classes
+    for (var i = 0 ; i < timeBlockElements.length ; i++) {
+
+        //Get element i's ID as a string
+        var elementID = timeBlockElements[i].id;
+
+        //get element by ID
+        var manipID = document.getElementById(timeBlockElements[i].id)
+
+        //remove any old classes from element
+        $(timeBlockElements[i].id).removeClass(".present .past .future");
+
+        // apply new class if task is present/past/future
+        if (elementID < currentTime) {
+            $(manipID).addClass("past");
+        } else if (elementID > currentTime) {
+            $(manipID).addClass("future");
+        } else {
+            $(manipID).addClass("present");
+        }
+    }
+}
+
+// checkTime every 5 minutes
+setInterval(checkTime(), (1000 * 60) * 5);
