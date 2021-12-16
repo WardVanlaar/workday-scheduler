0085ca54395ddb66ab1c3d9f3be51aa3192fe719
Ward <wardv@tirf.ca>
2021-12-16 13:56:02 -0500

    finished cleaning up code

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,8 +7,6 @@ let year = d.getFullYear();
 let month = d.getMonth()+1;
 let day = d.getDate();
 
-    //let totalDays = new Date(year, month, 0).getDate();
-
 let output = (month<10 ? '0' : '') + month + '/' + (day<10 ? '0' : '') + day + '/' + year;
 
 $("#currentDay").text(`${output}`);
